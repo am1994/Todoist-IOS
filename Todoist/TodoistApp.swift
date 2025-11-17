bcc8fd08ffc5fe59172a51b20abfe8581a8439b5
Amina Bekir <amina.bekir@potatolondon.com>
2025-11-17 10:50:36 +0100

    Initial Commit

---
+++ Todoist/TodoistApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoistApp.swift
+//  Todoist
+//
+//  Created by amina bekir on 17/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct TodoistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TodoistTests/TodoistTests.swift
@@ -0,0 +1,17 @@
+//
+//  TodoistTests.swift
+//  TodoistTests
+//
+//  Created by amina bekir on 17/11/2025.
+//
+
+import Testing
+@testable import Todoist
+
+struct TodoistTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
